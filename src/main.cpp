509a28d9e78f0ad83386aaf8eedfb7ab97674e3a
acesuiss <113465970+acesuiss@users.noreply.github.com>
2023-02-25 16:18:42 +0530

    All the new Function names are Renamend and Build Succesfully

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,5 @@
 #include <Arduino.h>
-#include<stdio.h>
+#include <stdio.h>
 /*******************************************************************************************
 *                                                                                         *
               Project Title : Automated Billing , Base Code
@@ -9,6 +9,7 @@
               Essentials    : Blynk_iot Accouct And blynk Libriaries
 *                                                                                         *
 *******************************************************************************************
+
 *********** defining Required blynk iot Libraries ********** */
 
 
@@ -25,10 +26,10 @@
 
 /* ********** Defining the Required pins ********** */
 
-#define Relay1  16   //d0
-#define Relay2  5    //d1
-#define Relay3  4    //d2
-#define Relay4  0    //d3
+#define Device1  16   //d0
+#define Device2  5    //d1
+#define Device3  4    //d2
+#define Device4  0    //d3
 
 #define switch1 2    //d4
 #define switch2 14   //d5
@@ -38,20 +39,21 @@
 
 /* ********** Function Prototype ********** */
 
-void Relaytime_data (struct time * device);
+void Collecte_Device_Timing (struct Device_Data * device);
 
-void sence_ace();
-void process_ace();
-void calculate(struct time * str);
-void broadcast_ace();
-/* ********** Structure To Store the Active Time  ********** */
+void sense_the_Switch();
+void Analyse_Each_Devices();
+void calculate(struct Device_Data * str);
+void Broadcast_the_Data();
+
 
 /*const int slab_1[4]=[100,100,200,100];
 const int rate_1[4]=[0,2.25,4.5,6];
 const int slab_2[4]=[100,100,200,100];
 float rate_2[4]=[0,2.25,4.5,6];*/
 
-struct time {
+/* ********** Structure To Store the Active Time  ********** */
+struct Device_Data {
   int sec =       0;
   int minute =     0;
   int hour =      0;
@@ -63,7 +65,7 @@ struct time {
 
 /* ********** Time Data for four devices ********** */
 
-struct time device1, device2, device3, device4;
+struct Device_Data Device1_Data, Device2_Data, Device3_Data, Device4_Data;
 
 
 void setup()
@@ -71,10 +73,10 @@ void setup()
   Serial.begin(115200);
   delay(100);
   /* ********** Setting the Relays as Outputs ********** */
-  pinMode(Relay1, OUTPUT);
-  pinMode(Relay2, OUTPUT);
-  pinMode(Relay3, OUTPUT);
-  pinMode(Relay4, OUTPUT);
+  pinMode(Device1, OUTPUT);
+  pinMode(Device2, OUTPUT);
+  pinMode(Device3, OUTPUT);
+  pinMode(Device4, OUTPUT);
   /* ********** Setting the Switches as Inputs ********** */
   pinMode(switch1, INPUT);
   pinMode(switch2, INPUT);
@@ -87,56 +89,57 @@ void setup()
 void loop() {
 
   BlynkEdgent.run();
-  sence_ace();
-  process_ace();
-  broadcast_ace();
+  sense_the_Switch();
+  Analyse_Each_Devices();
+  Broadcast_the_Data();
+  delay(950);
 
 }
 
 
 /* ********** check function to check each switch and update the relay and the timing  ********** */
 
-void sence_ace() {
+void sense_the_Switch() {
   //Serial.println("Entering the check function.  ");
 
   //Device1
   if (digitalRead(switch1) == HIGH)
   {
-    digitalWrite(Relay1, LOW);
-    Relaytime_data(&device1);
-  } else digitalWrite(Relay1, HIGH);
+    digitalWrite(Device1, LOW);
+    Collecte_Device_Timing(&Device1_Data);
+  } else digitalWrite(Device1, HIGH);
   //Device2
   if (digitalRead(switch2) == HIGH)
   {
-    digitalWrite(Relay2, LOW);
-    Relaytime_data(&device2);
-  } else digitalWrite(Relay2, HIGH);
+    digitalWrite(Device2, LOW);
+    Collecte_Device_Timing(&Device2_Data);
+  } else digitalWrite(Device2, HIGH);
   //Device3
   if (digitalRead(switch3) == HIGH)
   {
-    digitalWrite(Relay3, LOW);
-    Relaytime_data(&device3);
-  } else digitalWrite(Relay3, HIGH);
+    digitalWrite(Device3, LOW);
+    Collecte_Device_Timing(&Device3_Data);
+  } else digitalWrite(Device3, HIGH);
   //Device4
   if (digitalRead(switch4) == HIGH)
   {
-    digitalWrite(Relay4, LOW);
-    Relaytime_data(&device4);
-  } else digitalWrite(Relay1, HIGH);
+    digitalWrite(Device4, LOW);
+    Collecte_Device_Timing(&Device4_Data);
+  } else digitalWrite(Device1, HIGH);
 }
 
-void process_ace() {
-  calculate(&device1);
-  calculate(&device2);
-  calculate(&device3);
-  calculate(&device4);
+void Analyse_Each_Devices() {
+  calculate(&Device1_Data);
+  calculate(&Device2_Data);
+  calculate(&Device3_Data);
+  calculate(&Device4_Data);
 }
 
 
 
 /* ********** To update the timing of each device in  hours minute sec total sec format for  calculation  ********** */
 
-void Relaytime_data (struct time * device)
+void Collecte_Device_Timing (struct Device_Data * device)
 {
   device->sec++;
   device->total_sec++;
@@ -150,21 +153,21 @@ void Relaytime_data (struct time * device)
   }
 }
 
-void calculate(struct time * str) {
+void calculate(struct Device_Data * str) {
   double timeinhour = str->total_sec / 3600 ;
   str->total_amount = str->power * timeinhour ;
   //unsigned int temp_units = str->units;
 }
 
-void broadcast_ace() {
+void Broadcast_the_Data() {
 
-  Blynk.virtualWrite(V10, device1.units);     // virtual pin datas
-  Blynk.virtualWrite(V11, device2.units);
-  Blynk.virtualWrite(V12, device3.units);
-  Blynk.virtualWrite(V13, device4.units);
+  Blynk.virtualWrite(V10, Device1_Data.units);     // virtual pin datas
+  Blynk.virtualWrite(V11, Device2_Data.units);
+  Blynk.virtualWrite(V12, Device3_Data.units);
+  Blynk.virtualWrite(V13, Device4_Data.units);
 
-  Blynk.virtualWrite(V14, device1.total_amount);
-  Blynk.virtualWrite(V15, device2.total_amount);
-  Blynk.virtualWrite(V16, device3.total_amount);
-  Blynk.virtualWrite(V17, device4.total_amount);
+  Blynk.virtualWrite(V14, Device1_Data.total_amount);
+  Blynk.virtualWrite(V15, Device2_Data.total_amount);
+  Blynk.virtualWrite(V16, Device3_Data.total_amount);
+  Blynk.virtualWrite(V17, Device4_Data.total_amount);
 }
